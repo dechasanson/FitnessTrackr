2e98a5e40154db1ac6db4507d58d9d9c2927da49
deCha Sanson <deCha92@hotmail.com>
2020-11-30 20:06:40 -0600

    built the auth.js component and functions for logging in and registering

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { Activities, Routines } from "./Components";
+import { Activities, Routines, Auth } from "./Components";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { clearToken, getToken } from "./api";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(!!getToken());
   return (
     <div className="App">
       <header>
-        <h1>Welcome to the Fitness Tracker</h1>
+        <h1>Welcome to Fitness Tracker</h1>
         <nav>
           <Link to="/">Home</Link>
           <Link to="/Components/Activities">Activities</Link>
@@ -17,6 +19,26 @@ const App = () => {
       <main>
         <Route path="/">
           <h1>Home</h1>
+          {isLoggedIn ? (
+            <>
+              <div className="logout">
+                <h1 className="loginMessage">Thanks for logging in!</h1>
+                <span>
+                  <button
+                    className="logout-button"
+                    onClick={() => {
+                      clearToken();
+                      setIsLoggedIn(false);
+                    }}
+                  >
+                    LOG OUT
+                  </button>
+                </span>
+              </div>
+            </>
+          ) : (
+            <Auth setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} />
+          )}
         </Route>
         <Route
           path="/Components/Activities"
